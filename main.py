40f53cb15f81034c9c86d3d10e84ea83edea6a1e
Nandhini-Ammaiappan <24f1002469@ds.study.iitm.ac.in>
2025-09-17 12:09:16 +0530

    Create main.py
    New program first time commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#this is main module to do the mathematical operation
+
+a = 5
+b = 10
+
+sum = add(a,b)
+diff = sub(a,b)
+prod = mul(a,b)
+divison = div(b,a)
+
+print("Sum of two numbers is: ",sum)
+print("Difference of two numbers is: ",diff)
+print("Product of two numbers is: ",prod)
+print("Division of two numbers is: ",division)
